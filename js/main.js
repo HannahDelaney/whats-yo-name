fc488750a7549c3da6a4b446fa470779fae8e6a6
Hannah Brown <hannah.brown02@gmail.com>
2020-03-03 09:40:22 -0500

    Changed the alert to satisfy the machine
    even though my name is not Thomas

--- js/main.js
+++ js/main.js
@@ -3,5 +3,5 @@ var newName = prompt('What is your name?');
 if (newName == 'Hannah') {
   alert('Names are the same!');
 } else {
-  alert('Too bad you name is not Hannah, Thomas.');
+  alert('Names are different.');
 }
